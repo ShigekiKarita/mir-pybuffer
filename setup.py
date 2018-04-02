739f49ab4fe140316ffdbcf18da2030aa4a1f399
karita <shigekikarita@gmail.com>
2018-04-03 02:58:05 +0900

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pybuffer',
-    version='0.1.3',
+    version='0.1.4',
     description='pybuffer utility for ctypes',
     url='https://github.com/ShigekiKarita/mir-pybuffer',
     author='Shigeki Karita',
